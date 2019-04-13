5ce34bd136dd9e9774105379e11de65fa05e6cf7
Beder Acosta Borges <bedermail@yahoo.com>
2019-04-13 17:40:29 +0200

    Add nav bar and event card

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <h1>Events</h1>
-
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-      >Event 1</router-link
-    >
-    <router-link :to="{ name: 'event-show', params: { id: '2' } }"
-      >Event 2</router-link
-    >
-    <router-link :to="{ name: 'event-show', params: { id: '3' } }"
-      >Event 3</router-link
-    >
+    <EventCard />
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
