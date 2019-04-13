313e5a3bfacb32c7352b77c06e7139bc42d37c26
Beder Acosta Borges <bedermail@yahoo.com>
2019-04-13 16:36:10 +0200

    Dynamic routes and history mode

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Events</h1>
+  <div>
+    <h1>Events</h1>
+
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
+      >Event 1</router-link
+    >
+    <router-link :to="{ name: 'event-show', params: { id: '2' } }"
+      >Event 2</router-link
+    >
+    <router-link :to="{ name: 'event-show', params: { id: '3' } }"
+      >Event 3</router-link
+    >
+  </div>
 </template>
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Event</h1>
+  <h1>Event #{{ id }}</h1>
 </template>
+<script>
+export default {
+  props: ['id']
+}
+</script>
